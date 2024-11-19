9d6b18feaff7c5f7d05d65dcacb7beada78e9f56
Sergey87 <serge.techno@gmail.com>
2024-11-20 00:01:40 +0300

    added react router in project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import Navbar from './components/Navbar';
+import BuyCredit from './pages/BuyCredit';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <></>;
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
