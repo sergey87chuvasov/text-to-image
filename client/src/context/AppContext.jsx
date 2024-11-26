7f07667ce26f2e8606276b27eb797cacc6300ad7
Sergey87 <serge.techno@gmail.com>
2024-11-27 00:03:04 +0300

    added buycredit page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // false
 
   const value = {
     user,
