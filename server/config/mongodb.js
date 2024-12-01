632bfd555615b5de475456cc4fbfbfeff01d7e50
Sergey87 <serge.techno@gmail.com>
2024-12-01 15:35:34 +0300

    work with backend - 1 part

---
+++ server/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => {
+    console.log('DB CONNECTED!');
+  });
+  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
+};
+
+export default connectDB;
---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+import userModel from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+const registerUser = async (req,res) => {
+  try {}
+  catch(){}
+}
