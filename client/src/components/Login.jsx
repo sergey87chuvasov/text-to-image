e53fdb69d4bade52201a80bfe116bf3eb4f025f3
Sergey87 <serge.techno@gmail.com>
2024-12-06 00:01:00 +0300

    Integrated API calls for login and signup in the app.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,11 +2,41 @@ import { useContext, useEffect, useState } from 'react';
 import { assets } from '../assets/assets';
 import { AppContext } from '../context/AppContext';
 import { motion } from 'framer-motion';
+import axios from 'axios';
 
 const Login = () => {
   const [state, setState] = useState('Login');
+  const { setShowLogin, backendUrl, setToken, setUser } =
+    useContext(AppContext);
 
-  const { setShowLogin } = useContext(AppContext);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (state === 'Login') {
+        const { data } = await axios.post(backendUrl + '/api/user/login', {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+
+          localStorage.setItem('token', data.token);
+
+          setShowLogin(false);
+        } else {
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -18,6 +48,7 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       <motion.form
+        onSubmit={onSubmitHandler}
         className='relative bg-white p-10 rounded-xl text-slate-500'
         initial={{ opacity: 0.2, y: 50 }}
         transition={{ duration: 0.3 }}
@@ -31,6 +62,8 @@ const Login = () => {
           <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.user_icon} alt='icon pic' />
             <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
               type='text'
               className='outline-none text-sm'
               placeholder='Full Name'
@@ -41,6 +74,8 @@ const Login = () => {
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
           <img src={assets.email_icon} alt='icon pic' />
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             type='email'
             className='outline-none text-sm'
             placeholder='Email Id'
@@ -50,6 +85,8 @@ const Login = () => {
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
           <img src={assets.lock_icon} alt='icon pic' />
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             type='password'
             className='outline-none text-sm'
             placeholder='Password'
